7625aa54156ac2334d6fb19952db9feebed344f7
Luiz Felipe <raziel_libertino@hotmail.com>
2019-01-29 22:23:51 -0400

    Selando a classe SavingsAccount

---
+++ Herança_e_Polimorfismo/Entities/SavingsAccountPlus.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Herança_e_Polimorfismo.Entities
+{
+    class SavingsAccountPlus : SavingsAccount
+    { 
+    }
+}
