f992ecf20285144df2953251e1038ecc6bfefcf1
Luiz Felipe <raziel_libertino@hotmail.com>
2019-01-30 13:47:52 -0400

    Criado a classe ImportedProduct

---
+++ ExercicioPraFixacao/Entities/ImportedProduct.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioPraFixacao.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct()
+        {
+        }
+
+        public ImportedProduct(string name, double price, double customsFee) : base(name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public override string PriceTag()
+        {
+            return base.PriceTag()+"Customs fee: $ "+TotalPrice();
+        }
+
+        public double TotalPrice()
+        {
+            Price += CustomsFee;
+            return Price;
+
+        }
+    }
+}
