14e250521e806940e409ea7ce3162fee1d905152
Luiz Felipe <raziel_libertino@hotmail.com>
2019-01-30 13:40:06 -0400

    Criado a classe pai Product do exercicio de fixacao

---
+++ ExercicioPraFixacao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercicioPraFixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
