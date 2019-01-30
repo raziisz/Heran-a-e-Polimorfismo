5183d4c31d4b2baa1ebc0dc950e3c1b40691aef0
Luiz Felipe <raziel_libertino@hotmail.com>
2019-01-29 22:00:02 -0400

    Entendendo um Downcasting

--- Herança_e_Polimorfismo/Program.cs
+++ Herança_e_Polimorfismo/Program.cs
@@ -16,6 +16,27 @@ namespace Herança_e_Polimorfismo
             Account acc2 = new BusinessAcount(1003, "Bob", 0.0, 200.0);
             Account acc3 = new SavingsAccount(1004, "Ana", 0.0, 0.01);
 
+            //DOWNCASTING
+            BusinessAcount acc4 = (BusinessAcount)acc2;
+            acc4.Loan(100.0);
+
+            //BusinessAcount acc5 = (BusinessAcount)acc3;
+            if(acc3 is BusinessAcount)
+            {
+                //BusinessAcount acc5 = (BusinessAcount)acc3;
+                BusinessAcount acc5 = acc3 as BusinessAcount;
+                acc5.Loan(200.0);
+                Console.WriteLine("Loan!");
+            }
+
+            if(acc3 is SavingsAccount)
+            {
+                //SavingsAccount acc5 = (SavingsAccount)acc3;
+                SavingsAccount acc5 = acc3 as SavingsAccount;
+                acc5.UpdateBalance();
+                Console.WriteLine("Update!!!");
+            }
+
         }
     }
 }
