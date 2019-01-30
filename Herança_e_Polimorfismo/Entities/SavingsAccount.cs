d9e5d27dc561463509da21f9a7af0fc4924600c2
Luiz Felipe <raziel_libertino@hotmail.com>
2019-01-29 21:38:33 -0400

    Criado a classe SavingsAccount

---
+++ Herança_e_Polimorfismo/Entities/SavingsAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Herança_e_Polimorfismo.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate)
+            : base(number,holder,balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+    }
+}
