e06a119c9ffae7e9e3a7b21f53f36939517110ae
Luiz Felipe <raziel_libertino@hotmail.com>
2019-01-30 22:05:04 -0400

    Inicializado um exercício resolvido de metodo abstratos

---
+++ MetodoAbstrato/Entities/Shape.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetodoAbstrato.Entities
+{
+    abstract class Shape
+    {
+        public Color Color { get; set; }
+
+        public Shape()
+        {
+        }
+
+        public abstract double Area();
+        
+    }
+}
