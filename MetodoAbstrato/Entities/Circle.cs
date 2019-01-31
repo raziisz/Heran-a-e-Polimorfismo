c873abb18c182bcbe84d780a99f26f6aca328ad7
Luiz Felipe <raziel_libertino@hotmail.com>
2019-01-31 09:39:16 -0400

    Criado classe Circle e finalizada

---
+++ MetodoAbstrato/Entities/Circle.cs
@@ -0,0 +1,22 @@
+﻿
+namespace MetodoAbstrato.Entities
+{
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle()
+        {
+        }
+
+        public Circle(Color color, double radius) : base(color)
+        {
+            Radius = radius;
+        }
+
+        public override double Area()
+        {
+            return 3.1416 * (Radius * Radius);
+        }
+    }
+}
