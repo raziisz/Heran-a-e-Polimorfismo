64d572fb2e30b580ae0320b9ce94ce947aaac53c
Luiz Felipe <raziel_libertino@hotmail.com>
2019-01-31 10:02:24 -0400

    Finalizado exercicio resolvido de metodo abstrato e classe abstrata

--- MetodoAbstrato/Program.cs
+++ MetodoAbstrato/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MetodoAbstrato.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace MetodoAbstrato
 {
@@ -6,7 +9,47 @@ namespace MetodoAbstrato
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter the number of shapes: ");
+            int n = int.Parse(Console.ReadLine());
+            List<Shape> listShapes = new List<Shape>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Shape #{i} data:");
+                Console.Write("Rectangle or Circle (r/c)? ");
+                char type = char.Parse(Console.ReadLine());
+
+                Console.Write("Black/Blue/Red: ");
+                Color color = Enum.Parse<Color>(Console.ReadLine());
+
+                if(type == 'R' || type == 'r')
+                {
+                    Console.Write("Width: ");
+                    double width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.Write("Height: ");
+                    double heigth = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    listShapes.Add(new Rectangle(color, width, heigth));
+                } else if(type == 'C' || type == 'c'){
+                    Console.Write("Radius: ");
+                    double radius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    listShapes.Add(new Circle(color, radius));
+                } else
+                {
+                    Console.WriteLine("Comando invalido");
+                    i--;
+                }
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("SHAPE AREAS: ");
+
+            foreach (Shape shape in listShapes)
+            {
+                Console.WriteLine("Color: "+shape.Color+"Area: "+shape.Area().ToString("F2",CultureInfo.InvariantCulture));
+            }
         }
     }
 }
